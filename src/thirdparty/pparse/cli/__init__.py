a15c780d4a4ac614dd47a737382b8a0d13f42f69
chenz <crazychenz@gmail.com>
2026-03-16 10:33:57 +0000

    Added missing breakpoint param in pparse cli

--- src/thirdparty/pparse/cli/__init__.py
+++ src/thirdparty/pparse/cli/__init__.py
@@ -4,6 +4,11 @@ from thirdparty.pparse.cli.registry import get_commands, load_entrypoint_plugins
 def main():
     # Create parent parser
     parser = argparse.ArgumentParser(prog="pparse")
+    parser.add_argument("--breakpoint",
+        dest="breakpoint",
+        action="store_true",
+        help="breakpoint() after operation"
+    )
     subparsers = parser.add_subparsers(dest="pparse_command", required=True)
 
     # Load the entrypoints
